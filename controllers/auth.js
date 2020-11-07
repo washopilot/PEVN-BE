c820664b6e7e58c67945c8dc1fd41611234629ea
washopilot <washopilot@yahoo.com>
2020-11-06 21:15:26 -0500

    authentication.signIn post

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,4 +47,52 @@ authentication.signUp = async (req, res) => {
     }
 };
 
+authentication.signIn = async (req, res) => {
+    const { email, password, role } = req.body;
+    if (role == 'professor') {
+        try {
+            const professor = await (await pool.query('SELECT * FROM professor WHERE p_email=$1 AND p_password=$2', [email, password])).rows;
+            if (professor.length > 0) {
+                res.status(200).json({
+                    id: professor[0].id_p,
+                    name: professor[0].p_name,
+                    email: professor[0].p_email,
+                    role: 'professor',
+                });
+            } else {
+                res.status(200).json({
+                    message: 'El profesor no existe',
+                    NotFound: true,
+                })
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: 'Ha ocurrido otro error',
+                error
+            })
+        }
+    } else {
+        try {
+            const student = await (await pool.query('SELECT * FROM student WHERE s_email=$1 AND s_password=$2', [email, password])).rows;
+            if (student.length > 0) {
+                res.status(200).json({
+                    id: student[0].id_s,
+                    name: student[0].s_name,
+                    email: student[0].s_email,
+                    role: 'student',
+                });
+            } else {
+                res.status(200).json({
+                    message: 'El estudiante no existe',
+                    NotFound: true,
+                })
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: 'Ha ocurrido otro error',
+                error
+            })
+        }
+    }
+}
 module.exports = authentication;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/signup', authentication.signUp);
 
+router.post('/signin', authentication.signIn);
+
 module.exports = router;
